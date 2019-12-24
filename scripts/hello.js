4ba765a4c30b7e1f0b980dfce3094745a97ba549
undancer <undancer@gmail.com>
2019-12-25 01:20:01 +0800

    添加一个尝试性代码

---
+++ scripts/hello.js
@@ -0,0 +1,29 @@
+let {graphql, buildSchema} = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: [Hello]
+  }
+  type Hello {
+    id: ID!
+    name: String!
+  }
+`);
+
+let root = {
+    hello: () => ([{id: 1, name: 'name1'}, {id: 2, name: 'name2'}])
+};
+let query = `
+query {
+  hello {
+    id
+    name
+  }
+}`;
+
+graphql(schema, query, root).then((response) => {
+    response.data['hello'].forEach(({id, name}) => {
+        console.log(id);
+        console.log(name);
+    });
+});
