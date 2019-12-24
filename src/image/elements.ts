356649f5045c443cc6a51d1cbc40b6d19becc2f3
undancer <undancer@gmail.com>
2019-12-24 22:29:58 +0800

    添加资源，该资源通过指令产生

---
+++ src/image/elements.ts
@@ -0,0 +1,256 @@
+import _aluminumgas from './elements/aluminumgas.png';
+import _carbondioxide from './elements/carbondioxide.png';
+import _carbongas from './elements/carbongas.png';
+import _chlorinegas from './elements/chlorinegas.png';
+import _contaminatedoxygen from './elements/contaminatedoxygen.png';
+import _coppergas from './elements/coppergas.png';
+import _ethanolgas from './elements/ethanolgas.png';
+import _goldgas from './elements/goldgas.png';
+import _hydrogen from './elements/hydrogen.png';
+import _irongas from './elements/irongas.png';
+import _leadgas from './elements/leadgas.png';
+import _methane from './elements/methane.png';
+import _niobiumgas from './elements/niobiumgas.png';
+import _oxygen from './elements/oxygen.png';
+import _phosphorusgas from './elements/phosphorusgas.png';
+import _propane from './elements/propane.png';
+import _rockgas from './elements/rockgas.png';
+import _saltgas from './elements/saltgas.png';
+import _sourgas from './elements/sourgas.png';
+import _steam from './elements/steam.png';
+import _steelgas from './elements/steelgas.png';
+import _sulfurgas from './elements/sulfurgas.png';
+import _supercoolantgas from './elements/supercoolantgas.png';
+import _tungstengas from './elements/tungstengas.png';
+import _brine from './elements/brine.png';
+import _chlorine from './elements/chlorine.png';
+import _crudeoil from './elements/crudeoil.png';
+import _dirtywater from './elements/dirtywater.png';
+import _ethanol from './elements/ethanol.png';
+import _liquidcarbondioxide from './elements/liquidcarbondioxide.png';
+import _liquidhydrogen from './elements/liquidhydrogen.png';
+import _liquidmethane from './elements/liquidmethane.png';
+import _liquidoxygen from './elements/liquidoxygen.png';
+import _liquidphosphorus from './elements/liquidphosphorus.png';
+import _liquidpropane from './elements/liquidpropane.png';
+import _liquidsulfur from './elements/liquidsulfur.png';
+import _magma from './elements/magma.png';
+import _moltenaluminum from './elements/moltenaluminum.png';
+import _moltencarbon from './elements/moltencarbon.png';
+import _moltencopper from './elements/moltencopper.png';
+import _moltenglass from './elements/moltenglass.png';
+import _moltengold from './elements/moltengold.png';
+import _molteniron from './elements/molteniron.png';
+import _moltenlead from './elements/moltenlead.png';
+import _moltenniobium from './elements/moltenniobium.png';
+import _moltensalt from './elements/moltensalt.png';
+import _moltensteel from './elements/moltensteel.png';
+import _moltentungsten from './elements/moltentungsten.png';
+import _naphtha from './elements/naphtha.png';
+import _petroleum from './elements/petroleum.png';
+import _saltwater from './elements/saltwater.png';
+import _supercoolant from './elements/supercoolant.png';
+import _viscogel from './elements/viscogel.png';
+import _algae from './elements/algae.png';
+import _aluminum from './elements/aluminum.png';
+import _aluminumore from './elements/aluminumore.png';
+import _bitumen from './elements/bitumen.png';
+import _bleachstone from './elements/bleachstone.png';
+import _brineice from './elements/brineice.png';
+import _carbon from './elements/carbon.png';
+import _ceramic from './elements/ceramic.png';
+import _clay from './elements/clay.png';
+import _copper from './elements/copper.png';
+import _creature from './elements/creature.png';
+import _crushedice from './elements/crushedice.png';
+import _crushedrock from './elements/crushedrock.png';
+import _cuprite from './elements/cuprite.png';
+import _diamond from './elements/diamond.png';
+import _dirt from './elements/dirt.png';
+import _dirtyice from './elements/dirtyice.png';
+import _electrum from './elements/electrum.png';
+import _fertilizer from './elements/fertilizer.png';
+import _foolsgold from './elements/foolsgold.png';
+import _fossil from './elements/fossil.png';
+import _fullerene from './elements/fullerene.png';
+import _glass from './elements/glass.png';
+import _gold from './elements/gold.png';
+import _goldamalgam from './elements/goldamalgam.png';
+import _granite from './elements/granite.png';
+import _ice from './elements/ice.png';
+import _igneousrock from './elements/igneousrock.png';
+import _iron from './elements/iron.png';
+import _ironore from './elements/ironore.png';
+import _isoresin from './elements/isoresin.png';
+import _katairite from './elements/katairite.png';
+import _lead from './elements/lead.png';
+import _lime from './elements/lime.png';
+import _maficrock from './elements/maficrock.png';
+import _niobium from './elements/niobium.png';
+import _obsidian from './elements/obsidian.png';
+import _oxyrock from './elements/oxyrock.png';
+import _phosphorite from './elements/phosphorite.png';
+import _phosphorus from './elements/phosphorus.png';
+import _polypropylene from './elements/polypropylene.png';
+import _radium from './elements/radium.png';
+import _refinedcarbon from './elements/refinedcarbon.png';
+import _regolith from './elements/regolith.png';
+import _rust from './elements/rust.png';
+import _salt from './elements/salt.png';
+import _sand from './elements/sand.png';
+import _sandstone from './elements/sandstone.png';
+import _sedimentaryrock from './elements/sedimentaryrock.png';
+import _slimemold from './elements/slimemold.png';
+import _snow from './elements/snow.png';
+import _solidcarbondioxide from './elements/solidcarbondioxide.png';
+import _solidchlorine from './elements/solidchlorine.png';
+import _solidcrudeoil from './elements/solidcrudeoil.png';
+import _solidethanol from './elements/solidethanol.png';
+import _solidhydrogen from './elements/solidhydrogen.png';
+import _solidmethane from './elements/solidmethane.png';
+import _solidnaphtha from './elements/solidnaphtha.png';
+import _solidoxygen from './elements/solidoxygen.png';
+import _solidpetroleum from './elements/solidpetroleum.png';
+import _solidpropane from './elements/solidpropane.png';
+import _solidsupercoolant from './elements/solidsupercoolant.png';
+import _solidviscogel from './elements/solidviscogel.png';
+import _steel from './elements/steel.png';
+import _sulfur from './elements/sulfur.png';
+import _superinsulator from './elements/superinsulator.png';
+import _tempconductorsolid from './elements/tempconductorsolid.png';
+import _toxicsand from './elements/toxicsand.png';
+import _tungsten from './elements/tungsten.png';
+import _unobtanium from './elements/unobtanium.png';
+import _wolframite from './elements/wolframite.png';
+import _vacuum from './elements/vacuum.png';
+import _void from './elements/void.png';
+
+let elements = {
+    aluminumgas: _aluminumgas,
+    carbondioxide: _carbondioxide,
+    carbongas: _carbongas,
+    chlorinegas: _chlorinegas,
+    contaminatedoxygen: _contaminatedoxygen,
+    coppergas: _coppergas,
+    ethanolgas: _ethanolgas,
+    goldgas: _goldgas,
+    hydrogen: _hydrogen,
+    irongas: _irongas,
+    leadgas: _leadgas,
+    methane: _methane,
+    niobiumgas: _niobiumgas,
+    oxygen: _oxygen,
+    phosphorusgas: _phosphorusgas,
+    propane: _propane,
+    rockgas: _rockgas,
+    saltgas: _saltgas,
+    sourgas: _sourgas,
+    steam: _steam,
+    steelgas: _steelgas,
+    sulfurgas: _sulfurgas,
+    supercoolantgas: _supercoolantgas,
+    tungstengas: _tungstengas,
+    brine: _brine,
+    chlorine: _chlorine,
+    crudeoil: _crudeoil,
+    dirtywater: _dirtywater,
+    ethanol: _ethanol,
+    liquidcarbondioxide: _liquidcarbondioxide,
+    liquidhydrogen: _liquidhydrogen,
+    liquidmethane: _liquidmethane,
+    liquidoxygen: _liquidoxygen,
+    liquidphosphorus: _liquidphosphorus,
+    liquidpropane: _liquidpropane,
+    liquidsulfur: _liquidsulfur,
+    magma: _magma,
+    moltenaluminum: _moltenaluminum,
+    moltencarbon: _moltencarbon,
+    moltencopper: _moltencopper,
+    moltenglass: _moltenglass,
+    moltengold: _moltengold,
+    molteniron: _molteniron,
+    moltenlead: _moltenlead,
+    moltenniobium: _moltenniobium,
+    moltensalt: _moltensalt,
+    moltensteel: _moltensteel,
+    moltentungsten: _moltentungsten,
+    naphtha: _naphtha,
+    petroleum: _petroleum,
+    saltwater: _saltwater,
+    supercoolant: _supercoolant,
+    viscogel: _viscogel,
+    algae: _algae,
+    aluminum: _aluminum,
+    aluminumore: _aluminumore,
+    bitumen: _bitumen,
+    bleachstone: _bleachstone,
+    brineice: _brineice,
+    carbon: _carbon,
+    ceramic: _ceramic,
+    clay: _clay,
+    copper: _copper,
+    creature: _creature,
+    crushedice: _crushedice,
+    crushedrock: _crushedrock,
+    cuprite: _cuprite,
+    diamond: _diamond,
+    dirt: _dirt,
+    dirtyice: _dirtyice,
+    electrum: _electrum,
+    fertilizer: _fertilizer,
+    foolsgold: _foolsgold,
+    fossil: _fossil,
+    fullerene: _fullerene,
+    glass: _glass,
+    gold: _gold,
+    goldamalgam: _goldamalgam,
+    granite: _granite,
+    ice: _ice,
+    igneousrock: _igneousrock,
+    iron: _iron,
+    ironore: _ironore,
+    isoresin: _isoresin,
+    katairite: _katairite,
+    lead: _lead,
+    lime: _lime,
+    maficrock: _maficrock,
+    niobium: _niobium,
+    obsidian: _obsidian,
+    oxyrock: _oxyrock,
+    phosphorite: _phosphorite,
+    phosphorus: _phosphorus,
+    polypropylene: _polypropylene,
+    radium: _radium,
+    refinedcarbon: _refinedcarbon,
+    regolith: _regolith,
+    rust: _rust,
+    salt: _salt,
+    sand: _sand,
+    sandstone: _sandstone,
+    sedimentaryrock: _sedimentaryrock,
+    slimemold: _slimemold,
+    snow: _snow,
+    solidcarbondioxide: _solidcarbondioxide,
+    solidchlorine: _solidchlorine,
+    solidcrudeoil: _solidcrudeoil,
+    solidethanol: _solidethanol,
+    solidhydrogen: _solidhydrogen,
+    solidmethane: _solidmethane,
+    solidnaphtha: _solidnaphtha,
+    solidoxygen: _solidoxygen,
+    solidpetroleum: _solidpetroleum,
+    solidpropane: _solidpropane,
+    solidsupercoolant: _solidsupercoolant,
+    solidviscogel: _solidviscogel,
+    steel: _steel,
+    sulfur: _sulfur,
+    superinsulator: _superinsulator,
+    tempconductorsolid: _tempconductorsolid,
+    toxicsand: _toxicsand,
+    tungsten: _tungsten,
+    unobtanium: _unobtanium,
+    wolframite: _wolframite,
+    vacuum: _vacuum,
+    void: _void
+};
+export default elements;
