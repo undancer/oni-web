2515af4e1d8449b0916b242d15118bf795af49ca
undancer <undancer@gmail.com>
2019-12-27 18:23:34 +0800

    不包含特殊品

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,10 +3,14 @@ import _ from "underscore";
 
 let elements = (state: string = "vacuum") => {
     console.log("elements");
-    return _.filter(_elements, item => {
-        return "vacuum" === state || state === item.State.toLowerCase();
-    });
-    // return _.where(_elements, {"State": ""});
+    return _.filter(_elements, item => (
+            (
+                "vacuum" === state
+                || state === item.State.toLowerCase()
+            )
+            && !_.include(item.Tags, 'Special')
+        )
+    );
 };
 
 let data = {
