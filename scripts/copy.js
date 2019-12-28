3677f4ac7f43b4b84cc3f267a4a985c9256f60fb
undancer <undancer@gmail.com>
2019-12-28 16:16:46 +0800

    修改复制资源的脚本

--- scripts/copy.js
+++ scripts/copy.js
@@ -18,28 +18,31 @@ let findFile = (root, ext = '.png') => {
     return list;
 };
 
-["elements","buildings"].forEach(value => {
-    let path = "/Users/undancer/oni/assets/" + value;
+["elements", "buildings"].forEach(value => {
+    let root = "/Users/undancer/oni";
+    let path = root + "/assets/" + value;
     let save = p.join(__dirname, "../src/image/" + value);
+
 
     if (!fs.existsSync(save)) {
         fs.mkdirSync(save, {recursive: true})
     }
     let files = findFile(path);
-    let targets = [];
+    let targets = {};
     files.forEach(file => {
         let name = p.basename(file, '.png').toLowerCase();
-        let target = p.join(save, name + '.png');
-        fs.copyFileSync(file, target);
-        targets.push(name);
+        let value = p.relative(root, file).toLowerCase();
+        let target = p.join(__dirname, '../src', value).toLowerCase();
+        // fs.copyFileSync(file, target);
+        Object.assign(targets, {[name]: value});
     });
 
     let lines = [];
 
-    targets.forEach(target => {
-        lines.push(`import _${target} from './${value}/${target}.png';`);
+    Object.entries(targets).forEach(([key, value]) => {
+        lines.push(`import _${key} from './${value}';`);
     });
-    lines.push(`let ${value} = { ${targets.map(target => `${target}:_${target}`).join(',')} };`);
+    lines.push(`let ${value} = { ${Object.keys(targets).map(target => `${target}:_${target}`).join(',')} };`);
     lines.push(`export default ${value};`);
     let file = p.join(save, `../${value}.ts`);
     fs.writeFileSync(file, lines.join('\n'));
