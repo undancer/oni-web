0ed3694d02ec88fb603e57fa5df5656363a27e03
undancer <undancer@gmail.com>
2019-12-28 16:42:04 +0800

    国际化信息位置移动

--- src/data/strings.ts
+++ src/data/strings.ts
@@ -1,7 +1,7 @@
-import en from '../strings/en.json';
-import ko from '../strings/ko.json';
-import ru from '../strings/ru.json';
-import zh_CN from '../strings/zh_CN.json';
+import en from '../assets/strings/en.json';
+import ko from '../assets/strings/ko.json';
+import ru from '../assets/strings/ru.json';
+import zh_CN from '../assets/strings/zh_CN.json';
 
 let languages = () => ({
     en,
